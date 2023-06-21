3330b05137136826902d44d9fe4178d752baf7de
samih713 <sakmohamed@gmail.com>
2023-06-21 13:30:08 +0400

    updating tokenizer

--- src/parser/tokenizer.c
+++ src/parser/tokenizer.c
@@ -1,85 +1,99 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tokenizer.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/18 17:45:50 by tanas             #+#    #+#             */
-/*   Updated: 2023/06/18 17:45:51 by tanas            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "../../include/tokenizer.h"
+#include "../../include/tokenizer/type.h"
 
-#include "minishell.h"
 
 /* define TEST_ALL if you want to test all*/
 
-/* DELIMITER_FLAG */
-#define INSIDE_QUOTES 0X01 /* 0000 0001 */
+char const whitespace[] = " \t\r\n\v";
+char const operators[]="<|>&;()";
 
-/* using bitfields
-- oring an OPTION  for exampe INSIDE_QUOTES sets on the flag
-	INSIDE_QUOTES | int delimter_flag */
-#define SET(delmiter, flag) ((delimter, |= (flag)))
+/* checks if a char is an operator if its returns the operator
+c has to be a pointer because it needs to move to check for >>
+'+' for >>
+'a' for anything thats not an operator
+! change function name
+*/
+static char find_type(char **s)
+{
+	int	i;
 
-/* finds and returns the next token in an input_command string, takes in the string and index pointer */
-// char *static_find_token(char *input_command, size_t *i, u_int8_t delimter_flag)
-// {
-// 	/* follows bash grammar rules for determining the meaning of each character based on a set
-// 	of quoting flags
-// 	f1 - inside quotes
-// 	f2 - inside double quotes
-// 	f3 - inside Dollar braces
-// 	f4 - inside  */
-// 	if (input_command[*i] == '\'')
-// 		SET()
-// }
+	while (not_whitespace(**s))
+	{
+		i = 0;
+		while (operators[i])
+		{
+			if (!**s)
+				return (0);
+			else if (**s == '>' && **(s + 1) == '>')
+			{
+				*s += 2;
+				return ('+');
+			}
+			if (!**s)
+				return (0);
+			else if (**s == operators[i])
+			{
+				**s += 1;
+				return (**s);
+			}
+			i++;
+		}
+		*s += 1;
+	}
+	return ('a');
+}
+/* Returns false if it is a white space, also false if its a null */
+static bool not_whitespace(char c)
+{
+	int	i;
 
-/* Create a t_token, an entry into token que */
-// static t_token *create_token(char *token)
-// {
-// 	t_token *new = malloc(sizeof(t_token));
-// 	new->token = token;
-// 	return (new);
-// }
+	i = 0;
+	while (whitespace[i]) {
+		if (!c || c == whitespace[i])
+			return (false);
+		i++;
+	}
+	return (true);
+}
 
-// /* Break up the input command into tokens, and places them in a FIFO queue
-// returns the queue (t_queue)*/
-// t_queue *tokenizer(char *input_command)
-// {
-// 	t_queue		*token_queue;
-// 	t_token		*token;
-// 	char		*s_token;
-// 	size_t		i;
-// 	u_int8_t	*delimter_flag = NULL;
+char get_token(char **input_command, char *token_end,
+char **q, char **eq)
+{
+	char	ret;
+	char	*s;
 
-// 	// token_queue = create_queue();
-// 	i = 0;
-// 	while (input_command[i])
-// 	{
-// 		// s_token = find_token(input_command, &i, delimter_flag);
-// 		// token = create_token(s_token);
-// 		enqueue(token, token_queue);
-// 	}
-// 	enqueue(NULL, token_queue); /* adding a null node to terminate the queue */
-// 	return (token_queue);
-// }
+	s = *input_command; /* storing the original start of the string */
+	while (s < token_end && not_whitespace(*s))
+		s++;
+	if (q)
+		*q = s;
+	ret = *s;
+	ret = find_type(&s);
+	if (eq)
+		*eq = s;
+	/* skip white spaces */
+	while (!not_whitespace(*s))
+		s++;
+	*input_command = s;
+	return (ret);
+}
 
-// #ifdef TEST_ALL
-// int main(int argc, char **argv)
-// {
-// 	int i = 0;
-// 	for (char *test_case = INPUT_TEST[i]; test_case; i++, test_case = INPUT_TEST[i])
-// 	{
-// 		t_token *token_queue_ll = tkn_queue(test_case);
-// 		printf("Test case no: %d\n", i + 1);
-// 		printf("(%s)\n", test_case);
-// 		for (t_token *temp = token_queue_ll; temp; temp = temp->next)
-// 			printf("%s\n", temp->token);
-// 		printf("______________________________\n");
-// 	}
-// }
-// #endif
+
+#ifdef TEST_ALL
+int main(int argc, char **argv)
+{
+	int i = 0;
+	for (char *test_case = INPUT_TEST[i]; test_case; i++, test_case = INPUT_TEST[i])
+	{
+		t_token *token_queue_ll = tkn_queue(test_case);
+		printf("Test case no: %d\n", i + 1);
+		printf("(%s)\n", test_case);
+		for (t_token *temp = token_queue_ll; temp; temp = temp->next)
+			printf("%s\n", temp->token);
+		printf("______________________________\n");
+	}
+}
+#endif
 
 #ifdef CASE_11
 int main(int argc, char **argv)
@@ -89,9 +103,7 @@ int main(int argc, char **argv)
 }
 #endif
 
-int print_hi(void)
-{
-	return printf("hello");
-}
-// // TODO case 30 "echo 'Hello' >> file.txt 2>&1" last token?
+
+// TODO case 30 "echo 'Hello' >> file.txt 2>&1" last token??
+// TODO maybe quotes are supposed to be part of the token or no? depends on how it will be passed on to execve
 
