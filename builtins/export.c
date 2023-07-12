f102b3be5c849dfdc8a68be01941050aa5213a4c
samih713 <sakmohamed@gmail.com>
2023-07-12 05:07:52 +0400

    export version 1 I don't know why i can assign new strings to the env without reassigning memory to it

---
+++ builtins/export.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabdelra <sabdelra@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 03:46:11 by sabdelra          #+#    #+#             */
+/*   Updated: 2023/07/12 05:06:12 by sabdelra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main(int argc, char **argv, char **envp)
+{
+	int	var_len;
+	int	i;
+
+	var_len = 0;
+	if (argc != 2)
+		return (0);
+	argv++;
+	while ((*argv)[var_len] && (*argv)[var_len] != '=') {
+		var_len++;
+		if (!((*argv)[var_len]))
+			return (0);
+	}
+	for (i = 0; envp[i]; i++){
+		if (!ft_strncmp(*argv, envp[i], var_len + 1))
+			break ;
+	}
+	if (!envp[i]) // this really shouldn't work
+		envp[i+1] = NULL;
+	envp[i] = *argv;
+	for (int i = 0; envp[i]; i++)
+		printf("%s\n", envp[i]);
+}
