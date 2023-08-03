2733904f9e1a1750a70f9ba2706d1c118d288889
samih713 <sakmohamed@gmail.com>
2023-08-03 18:42:28 +0400

    changed prompt output to stderr, since apparently thats what bash does

--- src/main.c
+++ src/main.c
@@ -10,64 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "../test/test.h"
+#include "minishell.h"
+#include <unistd.h>
 
-t_signal	g_signal;
-
+t_signal g_signal;
 
 #ifdef TEST
-t_cmd	*get_cmd(char **envp, char *line)
-{
-	t_cmd	*root;
-	char	*dir;
-	char	*prompt;
+t_cmd *get_cmd(char **envp, char *line) {
+  t_cmd *root;
+  char *dir;
+  char *prompt;
 
-	dir = getcwd(NULL, 0);
-	prompt = ft_bigjoin(6, MAGENTA_B, "ghost@shell:", BLUE, dir, " → ", WHITE);
-	line = readline(MAGENTA_B"ghost@shell → "WHITE);
-	free(prompt);
-	free(dir);
-	if (!line)
-		return (ft_exit(), NULL);
-	add_history(line);
-	root = parsecmd(line, envp);
-	test(root);
-	return (root);
+  dir = getcwd(NULL, 0);
+  prompt = ft_bigjoin(6, MAGENTA_B, "ghost@shell:", BLUE, dir, " → ", WHITE);
+  line = readline(MAGENTA_B "ghost@shell → " WHITE);
+  free(prompt);
+  free(dir);
+  if (!line)
+    return (ft_exit(), NULL);
+  add_history(line);
+  root = parsecmd(line, envp);
+  test(root);
+  return (root);
 }
 #else
-t_cmd	*get_cmd(char **envp, char *line)
-{
-	char	*dir;
-	char	*prompt;
-	t_cmd	*root;
+t_cmd *get_cmd(char **envp, char *line) {
+  char *dir;
+  char *prompt;
+  t_cmd *root;
 
-	dir = getcwd(NULL, 0);
-	prompt = ft_bigjoin(6, MAGENTA_B, "ghost@shell:", BLUE, dir, " → ", WHITE);
-	line = readline(prompt);
-	free(prompt);
-	free(dir);
-	if (!line)
-		return (ft_exit(), NULL);
-	add_history(line);
-	root = parsecmd(line, envp);
-	return (root);
+  dir = getcwd(NULL, 0);
+  prompt = ft_bigjoin(6, MAGENTA_B, "ghost@shell:", BLUE, dir, " → ", WHITE);
+  dup2(STDERR_FILENO, STDOUT_FILENO);
+  line = readline(prompt);
+  free(prompt);
+  free(dir);
+  if (!line)
+    return (ft_exit(), NULL);
+  add_history(line);
+  root = parsecmd(line, envp);
+  return (root);
 }
 #endif
 
-int	main(int argc, char **argv __attribute__((unused)), char **envp)
-{
-	char	*line;
+int main(int argc, char **argv __attribute__((unused)), char **envp) {
+  char *line;
 
-	if (argc != 1)
-		return (printf(RED_B"Error: %s\n"WHITE, strerror(E2BIG)), ERR_ARGS);
-	receive_signal();
-	while (g_signal.exit_status == 0)
-	{
-		line = NULL;
-		runcmd(get_cmd(envp, line), envp);
-		free(line);
-	}
-	// rl_clear_history();
-	return (EXIT_SUCCESS);
+  if (argc != 1)
+    return (printf(RED_B "Error: %s\n" WHITE, strerror(E2BIG)), ERR_ARGS);
+  receive_signal();
+  while (g_signal.exit_status == 0) {
+    line = NULL;
+    runcmd(get_cmd(envp, line), envp);
+    free(line);
+  }
+  // rl_clear_history();
+  return (EXIT_SUCCESS);
 }
